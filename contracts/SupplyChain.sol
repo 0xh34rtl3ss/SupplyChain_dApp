d265f92edec3509c0de30e84742864541bd13d56
me-ran <75988843+0xh34rtl3ss@users.noreply.github.com>
2023-01-03 22:10:27 +0800

    add change of ownership , delivery function

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -10,7 +10,7 @@ contract SimpleStorage {
     );
 
     event AllSenderInformation(
-        uint256 senderID,
+        address senderID,
         string senderName,
         string senderLocation
     );
@@ -18,35 +18,47 @@ contract SimpleStorage {
     address admin;
 
     struct Sender {
-        uint256 senderID;
+        address senderID;
         string senderName;
         string senderLocation;
+        bool registered;
     }
 
     struct Hub {
         address hubID;
         string hubName;
         string hubLocation;
+        bool registered;
     }
 
     struct Delivery {
         address deliveryID;
         string deliveryName;
         string deliveryLocation;
+        bool registered;
+
     }
 
     struct Parcel {
         address sender;
         address currentOwner;
+        address nextOwner;
         string tracking;
         string locationCreated;
+        bool delivered;
     }
 
     Parcel[] public allParcels;
     Sender[] public allSenders;
+    Hub[] public allHubs;
+    Delivery[] public allDelivery;
 
+
+    
     uint256 senderCount = 0;
     uint256 parcelCount = 0;
+    uint256 hubCount = 0;
+    uint256 deliveryCount = 0;
 
     constructor() {
         admin = msg.sender;
@@ -57,36 +69,75 @@ contract SimpleStorage {
         _;
     }
 
+    modifier senderIsAllowed {
+    for(uint i = 0; i < allSenders.length; i++) {
+        if(msg.sender == allSenders[i].senderID) {
+        _;
+        }
+    }
+    revert();
+    }
+
+
+//change of ownership (done), verify user , delivery (done)
+
+
     function registration(
         string memory _roles, //make sure on the front end, the options during sign up are 'seller' , 'hub' , or 'delivery'
         string memory _name,
         string memory _location
     ) public {
-        if (
-            keccak256(abi.encodePacked(_roles)) ==
-            keccak256(abi.encodePacked("sender"))
-        ) {
-            allSenders.push(
-                Sender({
-                    senderID: senderCount,
-                    senderName: _name,
-                    senderLocation: _location
-                })
-            );
+        if (keccak256(abi.encodePacked(_roles)) ==keccak256(abi.encodePacked("sender"))) {
+            allSenders.push(Sender({senderID:msg.sender,senderName:_name,senderLocation:_location, registered:false}));
+        }
+        else if(keccak256(abi.encodePacked(_roles)) == keccak256(abi.encodePacked("hub"))){
+            allHubs.push(Hub({hubID:msg.sender,hubName:_name,hubLocation:_location,registered:false}));
+        }
+        else if(keccak256(abi.encodePacked(_roles)) == keccak256(abi.encodePacked("delivery"))){
+            allDelivery.push(Delivery({deliveryID:msg.sender,deliveryName:_name,deliveryLocation:_location,registered:false}));
         }
     }
 
-    function addParcel(string memory _trackingID, string memory _location)
-        public
-    {
-        allParcels.push(
-            Parcel({
-                sender: msg.sender,
-                currentOwner: msg.sender,
-                tracking: _trackingID,
-                locationCreated: _location
-            })
-        );
+
+
+    function addParcel(string memory _trackingID, string memory _location, address _nextOwner) public{      
+        allParcels.push(Parcel({sender:msg.sender,currentOwner:msg.sender,nextOwner:_nextOwner,tracking:_trackingID,locationCreated:_location,delivered:false}));
+    }
+
+    function changeofOwnership(string memory _trackingID, address  _nextOwner) public{
+        uint256 index;
+        for (uint256 i = 0; i < allParcels.length; i++) {
+            if (keccak256(abi.encodePacked(allParcels[i].tracking)) == keccak256(abi.encodePacked(_trackingID))) {
+                index = i;
+                break;
+        }
+    }
+        // Update the next owner of the parcel
+        allParcels[index].nextOwner = address(_nextOwner);
+
+    }
+
+    function delivery(string memory _trackingID) public {
+
+        for (uint256 i = 0; i < allParcels.length; i++) {
+            if (keccak256(abi.encodePacked(allParcels[i].tracking)) == keccak256(abi.encodePacked(_trackingID))) {
+                allParcels[i].delivered = true;
+            }
+        }
+
+    }
+
+    function viewParcelinfo(string memory _tracking) public {
+        for (uint256 i = 0; i < allParcels.length; i++) {
+            if (keccak256(abi.encodePacked(allParcels[i].tracking)) == keccak256(abi.encodePacked(_tracking))) {
+                emit AllParcelInformation(
+                allParcels[i].sender,
+                allParcels[i].currentOwner,
+                allParcels[i].tracking,
+                allParcels[i].locationCreated
+            );
+            }
+        }
     }
 
     function printAllSenders() public isAdmin {
@@ -101,14 +152,16 @@ contract SimpleStorage {
     }
 
     function printAllParcels() public isAdmin {
-        for (uint256 i = 0; i < allParcels.length; i++) {
-            Parcel storage _parcel = allParcels[i];
-            emit AllParcelInformation(
+
+        for (uint i = 0; i < allParcels.length; i++) {
+        Parcel storage _parcel = allParcels[i];
+        emit AllParcelInformation(
                 _parcel.sender,
                 _parcel.currentOwner,
                 _parcel.tracking,
                 _parcel.locationCreated
             );
-        }
+
+    }
     }
 }
